8fcec048dc2f2ad6bced1ae27bb5c541ac7cedf1
Hillary <kofisetornyoobeng@gmail.com>
2025-07-06 16:02:33 +0000

    updates

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   let bg1 = document.querySelector(".hero-bg.current");
   let bg2 = document.querySelector(".hero-bg.next");
+
   const images = [
     "https://images.pexels.com/photos/7108701/pexels-photo-7108701.jpeg",
     "https://images.pexels.com/photos/3183197/pexels-photo-3183197.jpeg",
+    "https://images.pexels.com/photos/3183198/pexels-photo-3183198.jpeg",
+    "https://images.pexels.com/photos/3184300/pexels-photo-3184300.jpeg",
+    "https://images.pexels.com/photos/3183171/pexels-photo-3183171.jpeg",
   ];
+
   let index = 0;
 
   // Initial backgrounds
